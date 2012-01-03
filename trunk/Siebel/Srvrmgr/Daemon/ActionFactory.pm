0efb9a7d973a70e0ddf8a2569691b43dbbdd3f89
alceu.freitas.jr@gmail.com <alceu.freitas.jr@gmail.com@f8da17be-bb99-043e-0d47-acc943b59c7d>
2012-01-03 21:24:44 +0000

    -refactored code -fixed bugs -added classes for Action during Daemon execution -code now is running, but only for Win32 systems. A review of code to rip off child process (srvrmgr.exe) must be checked to see zombie processes are not being created -ListParser.pm should be reviewed to use a factory class for different output types

--- trunk/Siebel/Srvrmgr/Daemon/ActionFactory.pm
+++ trunk/Siebel/Srvrmgr/Daemon/ActionFactory.pm
@@ -4,4 +4,6 @@ use warnings;
 use strict;
 use MooseX::AbstractFactory;
 
+implementation_class_via sub { shift };
+
 1;
--- trunk/Siebel/Srvrmgr/Daemon/Condition.pm
+++ trunk/Siebel/Srvrmgr/Daemon/Condition.pm
@@ -1,8 +1,97 @@
 package Siebel::Srvrmgr::Daemon::Condition;
+
+=pod
+=head1 NAME
+
+Siebel::Srvrmgr::Daemon::Condition - object that checks which conditions should keep a Siebel::Srvrmgr::Daemon running
+
+=head1 SYNOPSIS
+
+    my $condition = Siebel::Srvrmgr::Daemon::Condition(
+        {
+            is_infinite    => $self->is_infinite(),
+            total_commands => scalar( @{ $self->commands() } )
+        }
+    );
+
+=cut
+
 use warnings;
 use strict;
 use Moose;
 use namespace::autoclean;
 
+has is_infinite    => ( isa => 'Bool', is => 'ro', required => 1 );
+has total_commands => ( isa => 'Int',  is => 'ro', required => 1 );
+has cmd_counter    => (
+    isa      => 'Int',
+    is       => 'ro',
+    required => 1,
+    writer   => '_set_cmd_counter',
+    reader   => 'get_cmd_counter',
+    default  => 0
+);
+
+sub check {
+
+    my $self = shift;
+
+    if ( $self->is_infinite() and ( $self->total_commands() > 1 ) ) {
+
+        if ( ( $self->get_cmd_counter() + 1 ) <=
+            ( $self->total_commands() - 1 ) )
+        {
+
+            $self->add_cmd_counter();
+            return 1;
+
+        }
+        else {
+
+            $self->reset_cmd_counter();
+
+            return 1;
+
+        }
+
+    }
+    elsif ( $self->is_infinite() ) {
+
+        return 1;
+
+    }
+    else {
+
+        return 0;
+
+    }
+
+}
+
+sub add_cmd_counter {
+
+    my $self = shift;
+
+    if ( ( $self->get_cmd_counter() + 1 ) <= ( $self->total_commands() - 1 ) ) {
+
+        $self->_set_cmd_counter( $self->get_cmd_counter() + 1 );
+
+    }
+    else {
+
+        die "Can't increment counter because maximum of commands is "
+          . $self->total_commands() . "\n";
+
+    }
+
+}
+
+sub reset_cmd_counter {
+
+    my $self = shift;
+
+    $self->_set_cmd_counter(0);
+
+}
 
 __PACKAGE__->meta->make_immutable;
