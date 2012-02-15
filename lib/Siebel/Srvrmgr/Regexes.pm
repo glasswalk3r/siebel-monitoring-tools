a895187cc350406b7861c194b2a6293ceeda5fc7
alceu.freitas.jr@gmail.com <alceu.freitas.jr@gmail.com@f8da17be-bb99-043e-0d47-acc943b59c7d>
2012-02-15 18:42:51 +0000

    added POD added Exporter usage

--- lib/Siebel/Srvrmgr/Regexes.pm
+++ lib/Siebel/Srvrmgr/Regexes.pm
@@ -1,10 +1,76 @@
 package Siebel::Srvrmgr::Regexes;
 
-use constant SRVRMGR_PROMPT => qr/^srvrmgr(\:\w+)?>\s$/;
+require Exporter;
+@ISA       = qw(Exporter);
+@EXPORT_OK = qw(SRVRMGR_PROMPT LOAD_PREF_RESP LOAD_PREF_CMD CONN_GREET);
 
-#srvrmgr> File: d:\\sea752\\client\\bin\\.Siebel_svrmgr.pref'
-use constant LOAD_PREF_RESP => qr/^srvrmgr(\:\w+)?>\sFile\:\s.*\.pref$/;
-use constant LOAD_PREF_CMD  => qr/^srvrmgr(\:\w+)?>\sload preferences$/;
-use constant CONN_GREET => qr/^Siebel\sEnterprise\sApplications\sSiebel\sServer\sManager\,\sVersion*/;
+=pod
+
+=head1 NAME
+
+Siebel::Srvmrgr::Regexes - common regexes used by Siebel::Srvrmgr
+
+=head1 SYNOPSIS
+
+	use Siebel::Srvrmgr::Regexes qw(SRVRMGR_PROMPT);
+
+	if($line =~ /SRVRMGR_PROMPT/) {
+	
+		#do something
+	
+	}
+
+=head1 DESCRIPTION
+
+This modules exports several pre-compiled regular expressions by demand.
+
+=head1 EXPORTS
+
+=head2 SRVRMGR_PROMPT
+
+Regular expression to match the C<srvrmgr> prompt, without any command or output. It will match the server name, if included.
+
+=cut
+
+sub SRVRMGR_PROMPT {
+
+    return qr/^srvrmgr(\:\w+)?>\s$/;
+
+}
+
+=pod
+
+=head2 LOAD_PREF_RESP
+
+Regular expression to match the C<load preferences> response once the command is submitted.
+
+=cut
+
+sub LOAD_PREF_RESP { return qr/^(srvrmgr(\:\w+)?>)?\s?File\:\s.*\.pref$/; }
+
+=pod
+
+=head2 LOAD_PREF_CMD
+
+Regular expression to match the C<load preferences> command when submitted.
+
+=cut
+
+sub LOAD_PREF_CMD { return qr/^(srvrmgr(\:\w+)?>)?\s?load preferences$/; }
+
+=pod
+
+=head2 CONN_GREET
+
+Regular expression to match the first line submitted by a Siebel enterprise when the C<srvrmgr> connects to it. It will look like something like this:
+
+	Siebel Enterprise Applications Siebel Server Manager, Version 8.0.0.7 [20426] LANG_INDEPENDENT
+
+=cut
+
+sub CONN_GREET {
+    return
+qr/^Siebel\sEnterprise\sApplications\sSiebel\sServer\sManager\,\sVersion*/;
+}
 
 1;
