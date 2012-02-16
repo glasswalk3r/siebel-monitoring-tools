a4f933cef9c9a6a143116b0fd84a9c548dec1138
alceu.freitas.jr@gmail.com <alceu.freitas.jr@gmail.com@f8da17be-bb99-043e-0d47-acc943b59c7d>
2012-02-16 04:36:48 +0000

    -added automated tests (100% ok)

---
+++ trunk/t/Test/OutputFactory.pm
@@ -0,0 +1,45 @@
+package Test::OutputFactory;
+
+use Test::Pod::Coverage;
+use Test::Most;
+use base 'Test::Class';
+
+sub class { 'Siebel::Srvrmgr::ListParser::OutputFactory' }
+
+sub startup : Tests(startup => 1) {
+    my $test = shift;
+    use_ok $test->class;
+}
+
+sub constructor : Tests(10) {
+
+    my $test  = shift;
+    my $class = $test->class;
+
+    can_ok( $class, 'new' );
+
+    can_ok( $class, 'create' );
+
+    dies_ok {
+        my $output =
+          $class->create( 'foobar',
+            { data_type => 'foobar', raw_data => [], cmd_line => '' } );
+    }
+    'the create method fail with an invalid class';
+
+    pod_coverage_ok( $class, "$class is Pod covered" );
+
+    ok( $class->can_create('list_comp'),     'list_comp is a valid type' );
+    ok( $class->can_create('list_params'),   'list_params is a valid type' );
+    ok( $class->can_create('list_comp_def'), 'list_comp_def is a valid type' );
+    ok( $class->can_create('greetings'),     'greetings is a valid type' );
+    ok( $class->can_create('list_comp_types'),
+        'list_comp_types is a valid type' );
+    ok(
+        $class->can_create('load_preferences'),
+        'load_preferences is a valid type'
+    );
+
+}
+
+1;
