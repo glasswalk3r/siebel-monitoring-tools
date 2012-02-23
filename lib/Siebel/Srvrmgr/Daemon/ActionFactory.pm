69feb31d651d8eac5c5b434c50ba7c66c00ea6fb
alceu.freitas.jr@gmail.com <alceu.freitas.jr@gmail.com@f8da17be-bb99-043e-0d47-acc943b59c7d>
2012-02-23 22:25:17 +0000

    -added POD -changed the API of ActionFactory to include Action subclasses outside the distribution namespace

--- lib/Siebel/Srvrmgr/Daemon/ActionFactory.pm
+++ lib/Siebel/Srvrmgr/Daemon/ActionFactory.pm
@@ -1,9 +1,84 @@
 package Siebel::Srvrmgr::Daemon::ActionFactory;
 
+=pod
+
+=head1 NAME
+
+Siebel::Srvrmgr::Daemon::ActionFactory - abstract factory to create Action subclasses
+
+=head1 SYNOPSIS
+
+	my $action = Siebel::Srvrmgr::Daemon::ActionFactory->create(
+		$class,
+		{
+			parser => Siebel::Srvrmgr::ListParser->new(),
+			params => \@params
+		}
+	);
+
+
+=head1 DESCRIPTION
+
+This is a abstract factory used to instatiate Action classes. It is used primarily by L<Siebel::Srvrmgr::Daemon> class
+to define the Action objects to process generated output.
+
+=cut
+
 use warnings;
 use strict;
 use MooseX::AbstractFactory;
 
-implementation_class_via sub { 'Siebel::Srvrmgr::Daemon::Action::' . shift };
+=pod
+
+=head1 METHODS
+
+=head2 create
+
+Expects as parameter the name of the class that needs to be instantiated followed by any required parameters for the class to 
+instantiate a new object. It returns the instantiated object, if possible, otherwise it will raise an exception.
+
+If a single string (without double semicolon separators) is given as the class name, ActionFactory will understand that it will
+have to expand it to a full Action subclass name available from this distribution. For example, if a "LoadPrefences" is given it
+will be expanded to "Siebel::Srvrmgr::Daemon::Action::LoadPreferences" and try to instantiate such object.
+
+If a full class name (with double semicolon separators) is given, the factory will try to instantiate that object and return it. That should
+make possible to create outsite the distribution directory.
+
+=cut
+
+implementation_class_via sub {
+
+    my $classname = shift;
+
+    if ( $classname =~ /\:{2}/ ) {
+
+        return $classname;
+
+    }
+    else {
+
+        return 'Siebel::Srvrmgr::Daemon::Action::' . $classname;
+
+    }
+
+};
+
+=pod
+
+=head1 SEE ALSO
+
+=over 2
+
+=item *
+
+L<Siebel::Srvrmgr::Daemon::Action>
+
+=item *
+
+L<MooseX::AbstractFactory>
+
+=back
+
+=cut
 
 1;
