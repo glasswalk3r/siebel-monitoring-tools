8d13cb90aa4af025c7dc13d95ccc499d187dd1ec
alceu.freitas.jr@gmail.com <alceu.freitas.jr@gmail.com@f8da17be-bb99-043e-0d47-acc943b59c7d>
2012-03-03 20:49:34 +0000

    initial release

---
+++ trunk/lib/Siebel/Srvrmgr.pm
@@ -0,0 +1,40 @@
+package Siebel::Srvrmgr;
+
+our $VERSION = '0.01';
+
+=pod
+
+=head1 NAME
+
+Siebel::Srvrmgr - utilities to be used with the Siebel srvrmgr program
+
+=head1 DESCRIPTION
+
+This package is only Pod, no code is available from here.
+
+The distribution Siebel-Srvrmgr was created to define a set of tools to interact with the C<srvrmgr> program from Siebel.
+
+It was started initially to create a parser for the project Siebel Monitoring Tools (L<< http://code.google.com/p/siebel-monitoring-tools/ >>) and latter was grown to a set of more generic functionality, thus
+making sense to be published at CPAN.
+
+=head1 SEE ALSO
+
+The classes below might give you a introduction of the available classes and features.
+
+=over 2
+
+=item *
+
+L<Siebel::Srvrmgr::Daemon>
+
+=item *
+
+L<Siebel::Srvrmgr::ListParser>
+
+=back
+
+=head1 AUTHOR
+
+Alceu Rodrigues de Freitas Junior, E<lt>arfreitas@cpan.org<E<gt>
+
+=cut
