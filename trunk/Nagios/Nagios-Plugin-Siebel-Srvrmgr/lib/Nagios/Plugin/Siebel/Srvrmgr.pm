cb884f3cea037f627d0d59bba19cc549d2bdcec2
alceu.freitas.jr <alceu.freitas.jr@f8da17be-bb99-043e-0d47-acc943b59c7d>
2013-10-21 14:45:52 +0000

    -added Pod to avoid warnings from Makefile.PL

--- trunk/Nagios/Nagios-Plugin-Siebel-Srvrmgr/lib/Nagios/Plugin/Siebel/Srvrmgr.pm
+++ trunk/Nagios/Nagios-Plugin-Siebel-Srvrmgr/lib/Nagios/Plugin/Siebel/Srvrmgr.pm
@@ -4,4 +4,34 @@ use 5.008009;
 
 our $VERSION = '0.01';
 
+=pod
+
+=head1 Name
+
+Nagios::Plugin::Siebel::Srvrmgr - Perl module to enable interaction between Siebel::Srvrmgr and Nagios as a plugin
+
+=head1 Description
+
+This module is only Pod regarding the distribution.
+
+The distribution Nagios-Plugin-Siebel-Srvrmgr has several modules used to integrate L<Siebel::Srvrmgr> API with Nagios by build plugins.
+
+This release includes implementations made as an XML-RPC server and an XML-RPC client, being the last the real Nagios plugin.
+
+=head1 SEE ALSO
+
+=over
+
+=item *
+
+L<Siebel::Srvrmgr>
+
+=item *
+
+L<Nagios::Plugin>
+
+=back
+
+=cut
+
 1;
