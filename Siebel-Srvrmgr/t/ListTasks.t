468b8f52908e59e3089a0c23f7e86a05423c8e02
alceu.freitas.jr@gmail.com <alceu.freitas.jr@gmail.com@f8da17be-bb99-043e-0d47-acc943b59c7d>
2014-01-04 19:44:53 +0000

    fixing due last API changes

--- Siebel-Srvrmgr/t/ListTasks.t
+++ Siebel-Srvrmgr/t/ListTasks.t
@@ -1,4 +1,21 @@
 use lib 't';
-use Test::Siebel::Srvrmgr::ListParser::Output::ListTasks;
+use Test::Siebel::Srvrmgr::ListParser::Output::Tabular::ListTasks;
+use File::Spec;
 
-Test::Class->runtests;
+my $fixed_test =
+  Test::Siebel::Srvrmgr::ListParser::Output::Tabular::ListTasks->new(
+    structure_type => 'fixed',
+    output_file =>
+      File::Spec->catfile( 't', 'output', 'fixed', 'list_tasks.txt' )
+  );
+
+my $delimited_test =
+  Test::Siebel::Srvrmgr::ListParser::Output::Tabular::ListTasks->new(
+    structure_type => 'delimited',
+    col_sep        => '|',
+    output_file =>
+      File::Spec->catfile( 't', 'output', 'delimited', 'list_tasks.txt' )
+  );
+
+Test::Class->runtests( $fixed_test, $delimited_test );
+
--- Siebel-Srvrmgr/t/Test/Siebel/Srvrmgr/ListParser/Output/Tabular/ListTasks.pm
+++ Siebel-Srvrmgr/t/Test/Siebel/Srvrmgr/ListParser/Output/Tabular/ListTasks.pm
@@ -1,9 +1,7 @@
 package Test::Siebel::Srvrmgr::ListParser::Output::Tabular::ListTasks;
 
 use Test::Most;
-use Siebel::Srvrmgr::ListParser::Output::ListTasks::Task;
-use Siebel::Srvrmgr::ListParser::Output::ListTasks;
-use base qw(Test::Siebel::Srvrmgr::ListParser::Output::Tabular);
+use parent qw(Test::Siebel::Srvrmgr::ListParser::Output::Tabular);
 
 sub get_data_type {
 
@@ -17,69 +15,50 @@ sub get_cmd_line {
 
 }
 
-sub class_methods : Tests(+5) {
+sub class_methods : Tests(no_plan) {
 
     my $test = shift;
 
-    my $parsed_data = {
-        'siebfoobar1' => [
-            Siebel::Srvrmgr::ListParser::Output::ListTasks::Task->new(
-                {
-                    'comp_alias'  => 'ServerMgr',
-                    'pid'         => '6302',
-                    'status'      => 'Running',
-                    'id'          => '132120579',
-                    'server_name' => 'siebfoobar1'
-                }
-            ),
-            Siebel::Srvrmgr::ListParser::Output::ListTasks::Task->new(
-                {
-                    'comp_alias'  => 'eCommunicationsObjMgr_ptb',
-                    'pid'         => '27726',
-                    'status'      => 'Completed',
-                    'id'          => '131072058',
-                    'server_name' => 'siebfoobar1'
-                }
-            ),
-        ],
-        'siebfoobar2' => [
-            Siebel::Srvrmgr::ListParser::Output::ListTasks::Task->new(
-                {
-                    'comp_alias'  => 'ServerMgr',
-                    'pid'         => '5902',
-                    'status'      => 'Running',
-                    'id'          => '130023426',
-                    'server_name' => 'siebfoobar2'
-                }
-            ),
-            Siebel::Srvrmgr::ListParser::Output::ListTasks::Task->new(
-                {
-                    'comp_alias'  => 'eCommunicationsObjMgr_ptb',
-                    'pid'         => '5464',
-                    'status'      => 'Running',
-                    'id'          => '128975007',
-                    'server_name' => 'siebfoobar2'
-                }
-            ),
-            Siebel::Srvrmgr::ListParser::Output::ListTasks::Task->new(
-                {
-                    'comp_alias'  => 'eChannelCMEObjMgr_ptb',
-                    'pid'         => '5364',
-                    'status'      => 'Completed',
-                    'id'          => '127926815',
-                    'server_name' => 'siebfoobar2'
-                }
-            ),
-        ]
-    };
+    $test->SUPER::class_methods( [qw(get_servers get_tasks)] );
+
+	$DB::single = 1;
+
+    $test->num_tests(
+        scalar( @{ $test->get_output()->get_data_parsed()->{siebel1} } ) + 6 );
 
     ok( $test->get_output()->get_data_parsed(), 'get_data_parsed works' );
 
+    my $expected;
+
+    if ( $test->get_output()->get_type() eq 'fixed' ) {
+
+        $expected = $test->get_expected_fixed();
+
+    }
+    else {
+
+        $expected = $test->get_expected_del();
+
+    }
+
     cmp_deeply(
-        $parsed_data,
+        $expected,
         $test->get_output()->get_data_parsed(),
         'get_data_parsed() returns the correct data structure'
     );
+
+    cmp_deeply( $test->get_output()->get_servers(),
+        (qw(siebel1)), 'get_servers() returns the expected value' );
+
+    my $next_task = $test->get_output()->get_tasks('siebel1');
+
+    is( ref($next_task), 'CODE', 'get_tasks returns a code reference' );
+
+    while ( my $task = $next_task->() ) {
+
+        isa_ok( $task, 'Siebel::Srvrmgr::ListParser::Output::ListTasks::Task' );
+
+    }
 
     ok(
         $test->get_output()->set_data_parsed(
@@ -103,27 +82,256 @@ sub class_methods : Tests(+5) {
     dies_ok { $test->get_output()->set_data_parsed('foobar') }
     'set_data_parsed dies with incorrect parameters';
 
-# :TODO      :10/06/2013 18:09:50:: Siebel::Srvrmgr::ListParser::Output should be tested against undefined data reference to be parsed
-# after initial clean up
-    my @invalid_output =
-      ( 'foobar', 'bar', 'foo', 'floor', 'for', 'yadayadayada', 'garbage' );
+}
 
-    $test->get_output()->set_raw_data( \@invalid_output );
+sub get_expected_del {
 
-    dies_ok { $test->get_output()->parse() }
-    'dies with invalid output to be parsed due restricted fields/columns';
+    return {
+        'siebel1' => [
+            [
+                'siebel1',                         'ServerMgr',
+                '15728642',                        '6963',
+                'Running',                         'Interactive',
+                '2013-12-08 18:31:48',             '2000-00-00 00:00:00',
+                'Processing "List Tasks" command', 'System',
+                '',                                '0',
+                '',                                'Normal',
+                ''
+            ],
+            [
+                'siebel1',             'ServerMgr',
+                '14680066',            '6948',
+                'Completed',           'Interactive',
+                '2013-12-08 18:29:55', '2013-12-08 18:31:00',
+                '',                    'System',
+                '',                    '0',
+                '',                    'Normal',
+                ''
+            ],
+            [
+                'siebel1',             'ServerMgr',
+                '13631490',            '6932',
+                'Completed',           'Interactive',
+                '2013-12-08 18:25:32', '2013-12-08 18:28:31',
+                '',                    'System',
+                '',                    '0',
+                '',                    'Normal',
+                ''
+            ],
+            [
+                'siebel1',             'ServerMgr',
+                '12582914',            '6918',
+                'Completed',           'Interactive',
+                '2013-12-08 18:24:15', '2013-12-08 18:25:30',
+                '',                    'System',
+                '',                    '0',
+                '',                    'Normal',
+                ''
+            ],
+            [
+                'siebel1',
+                'ServerMgr',
+                '11534338',
+                '6910',
+                'Exited with error',
+                'Interactive',
+                '2013-12-08 18:23:50',
+                '2013-12-08 18:24:00',
+'SBL-SEC-10007: The password you have entered is not correct. Pl',
+                'System',
+                '',
+                '0',
+                '',
+                'Normal',
+                ''
+            ],
+            [
+                'siebel1',
+                'SvrTblCleanup',
+                '8388610',
+                '3294',
+                'Running',
+                'Background',
+                '2013-12-08 17:11:30',
+                '2000-00-00 00:00:00',
+'Method DelCompletedDelExpiredReq for service Message Board Maintenance Service has executed 16 times -- Sleeping',
+                'SystemAux',
+                '',
+                '0',
+                'SADMIN',
+                'Normal',
+                ''
+            ],
+            [
+                'siebel1',
+                'SvrTaskPersist',
+                '7340034',
+                '3257',
+                'Running',
+                'Background',
+                '2013-12-08 17:11:30',
+                '2000-00-00 00:00:00',
+'Method InsertUpdateTaskHistory for service Message Board Maintenance Service has executed 157 times -- Sleeping',
+                'SystemAux',
+                '',
+                '0',
+                'SADMIN',
+                'Normal',
+                ''
+            ],
+            [
+                'siebel1',             'SRProc',
+                '5242887',             '3258',
+                'Running',             'Interactive',
+                '2013-12-08 17:12:12', '2000-00-00 00:00:00',
+                '',                    'SystemAux',
+                '',                    '0',
+                '',                    'Normal',
+                ''
+            ],
+            [
+                'siebel1',             'SRProc',
+                '5242885',             '3258',
+                'Running',             'Interactive',
+                '2013-12-08 17:12:03', '2000-00-00 00:00:00',
+                '',                    'SystemAux',
+                '',                    '0',
+                'Forwarding Task',     'Worker',
+                ''
+            ],
+            [
+                'siebel1',             'SCBroker',
+                '3145730',             '3226',
+                'Running',             'Background',
+                '2013-12-08 17:11:25', '2000-00-00 00:00:00',
+                '',                    'System',
+                '',                    '0',
+                '',                    'Normal',
+                ''
+            ],
+            [
+                'siebel1',             'SRBroker',
+                '2097167',             '3225',
+                'Running',             'Interactive',
+                '2013-12-08 17:12:15', '2000-00-00 00:00:00',
+                '',                    'System',
+                '',                    '0',
+                '',                    'Normal',
+                ''
+            ],
+            [
+                'siebel1',             'SRBroker',
+                '2097166',             '3225',
+                'Running',             'Interactive',
+                '2013-12-08 17:12:15', '2000-00-00 00:00:00',
+                '',                    'System',
+                '',                    '0',
+                '',                    'Normal',
+                ''
+            ],
+            [
+                'siebel1',             'SRBroker',
+                '2097165',             '3225',
+                'Running',             'Interactive',
+                '2013-12-08 17:12:12', '2000-00-00 00:00:00',
+                '',                    'System',
+                '',                    '0',
+                '',                    'Normal',
+                ''
+            ],
+            [
+                'siebel1',             'SRBroker',
+                '2097164',             '3225',
+                'Running',             'Interactive',
+                '2013-12-08 17:12:12', '2000-00-00 00:00:00',
+                '',                    'System',
+                '',                    '0',
+                '',                    'Normal',
+                ''
+            ],
+            [
+                'siebel1',             'SRBroker',
+                '2097161',             '3225',
+                'Running',             'Interactive',
+                '2013-12-08 17:12:12', '2000-00-00 00:00:00',
+                '',                    'System',
+                '',                    '0',
+                'Response task',       'Worker',
+                ''
+            ],
+            [
+                'siebel1',             'SRBroker',
+                '2097160',             '3225',
+                'Running',             'Interactive',
+                '2013-12-08 17:12:12', '2000-00-00 00:00:00',
+                '',                    'System',
+                '',                    '0',
+                'Store task',          'Worker',
+                ''
+            ],
+            [
+                'siebel1',             'SRBroker',
+                '2097159',             '3225',
+                'Running',             'Interactive',
+                '2013-12-08 17:12:12', '2000-00-00 00:00:00',
+                '',                    'System',
+                '',                    '0',
+                '',                    'Normal',
+                ''
+            ],
+            [
+                'siebel1',             'SRBroker',
+                '2097158',             '3225',
+                'Running',             'Interactive',
+                '2013-12-08 17:12:12', '2000-00-00 00:00:00',
+                '',                    'System',
+                '',                    '0',
+                'Task creation task',  'Worker',
+                ''
+            ],
+            [
+                'siebel1',                  'SRBroker',
+                '2097157',                  '3225',
+                'Running',                  'Interactive',
+                '2013-12-08 17:12:12',      '2000-00-00 00:00:00',
+                '',                         'System',
+                '',                         '0',
+                'Information caching task', 'Worker',
+                ''
+            ]
+        ]
+    };
+
+}
+
+sub get_expected_fixed {
+
+    return {
+        'siebel1' => [
+            [ 'siebel1', 'ServerMgr', '16777218', '6974', 'Running' ],
+            [ 'siebel1', 'ServerMgr', '15728642', '6963', 'Completed' ],
+            [ 'siebel1', 'ServerMgr', '14680066', '6948', 'Completed' ],
+            [ 'siebel1', 'ServerMgr', '13631490', '6932', 'Completed' ],
+            [ 'siebel1', 'ServerMgr', '12582914', '6918', 'Completed' ],
+            [ 'siebel1', 'ServerMgr', '11534338', '6910', 'Exited with error' ],
+            [ 'siebel1', 'SvrTblCleanup',  '8388610', '3294', 'Running' ],
+            [ 'siebel1', 'SvrTaskPersist', '7340034', '3257', 'Running' ],
+            [ 'siebel1', 'SRProc',         '5242887', '3258', 'Running' ],
+            [ 'siebel1', 'SRProc',         '5242885', '3258', 'Running' ],
+            [ 'siebel1', 'SCBroker',       '3145730', '3226', 'Running' ],
+            [ 'siebel1', 'SRBroker',       '2097167', '3225', 'Running' ],
+            [ 'siebel1', 'SRBroker',       '2097166', '3225', 'Running' ],
+            [ 'siebel1', 'SRBroker',       '2097165', '3225', 'Running' ],
+            [ 'siebel1', 'SRBroker',       '2097164', '3225', 'Running' ],
+            [ 'siebel1', 'SRBroker',       '2097161', '3225', 'Running' ],
+            [ 'siebel1', 'SRBroker',       '2097160', '3225', 'Running' ],
+            [ 'siebel1', 'SRBroker',       '2097159', '3225', 'Running' ],
+            [ 'siebel1', 'SRBroker',       '2097158', '3225', 'Running' ],
+            [ 'siebel1', 'SRBroker',       '2097157', '3225', 'Running' ]
+          ]
+
+    };
 
 }
 
 1;
-__DATA__
-SV_NAME      CC_ALIAS                   TK_TASKID  TK_PID  TK_DISP_RUNSTATE
------------  -------------------------  ---------  ------  ----------------
-siebfoobar1  ServerMgr                  132120579  6302    Running
-siebfoobar1  eCommunicationsObjMgr_ptb  131072058  27726   Completed
-siebfoobar2  ServerMgr                  130023426  5902    Running
-siebfoobar2  eCommunicationsObjMgr_ptb  128975007  5464    Running
-siebfoobar2  eChannelCMEObjMgr_ptb      127926815  5364    Completed
-
-14 rows returned.
-
