3e39422ec8986f0165042880ec376aa0ecddc324
alceu.freitas.jr@gmail.com <alceu.freitas.jr@gmail.com@f8da17be-bb99-043e-0d47-acc943b59c7d>
2014-01-11 21:57:52 +0000

    more fixes dues API change

--- Siebel-Srvrmgr/t/Test/Siebel/Srvrmgr/Server.pm
+++ Siebel-Srvrmgr/t/Test/Siebel/Srvrmgr/Server.pm
@@ -2,15 +2,15 @@ package Test::Siebel::Srvrmgr::ListParser::Output::ListComp::Server;
 
 use Test::Most;
 use Test::Moose qw(has_attribute_ok);
-use base 'Test::Siebel::Srvrmgr';
-use Siebel::Srvrmgr::ListParser::Output::ListComp;
+use parent 'Test::Siebel::Srvrmgr';
+use Siebel::Srvrmgr::ListParser::Output::Tabular::ListComp;
 
 sub _constructor : Tests(+2) {
 
     my $test = shift;
 
-    #must parse the output from DATA handle
-    my $list_comp = Siebel::Srvrmgr::ListParser::Output::ListComp->new(
+    #must parse the output
+    my $list_comp = Siebel::Srvrmgr::ListParser::Output::Tabular::ListComp->new(
         {
             data_type => 'list_comp',
             raw_data  => $test->get_my_data(),
@@ -22,6 +22,20 @@ sub _constructor : Tests(+2) {
 
     ok( $test->{server}, 'the constructor should succeed' );
     isa_ok( $test->{server}, $test->class() );
+
+}
+
+# :TODO:11-01-2014:: refactor the method below because Tabular does the same (maybe a Role?)
+sub get_my_data {
+
+    my $test = shift;
+
+    my $data_ref = $test->SUPER::get_my_data();
+
+    shift( @{$data_ref} );    #command
+    shift( @{$data_ref} );    #new line
+
+    return $data_ref;
 
 }
 
@@ -49,11 +63,12 @@ sub class_methods : Tests(5) {
 
 }
 
-sub class_attributes : Tests(2) {
+sub class_attributes : Tests(no_plan) {
 
     my $test = shift;
 
-    my @attribs = qw(name data);
+    my @attribs = qw(name data comp_attribs);
+    $test->num_tests( scalar(@attribs) );
 
     foreach my $attrib (@attribs) {
 
@@ -64,41 +79,4 @@ sub class_attributes : Tests(2) {
 }
 
 1;
-
-__DATA__
-SV_NAME     CC_ALIAS           CC_NAME                                  CT_ALIAS  CG_ALIAS      CC_RUNMODE   CP_DISP_RUN_STATE  CP_NUM_RUN_TASKS  CP_MAX_TASKS  CP_ACTV_MTS_PROCS  CP_MAX_MTS_PROCS  CP_START_TIME        CP_END_TIME  CP_STATUS  CC_INCARN_NO  CC_DESC_TEXT  
-----------  -----------------  ---------------------------------------  --------  ------------  -----------  -----------------  ----------------  ------------  -----------------  ----------------  -------------------  -----------  ---------  ------------  ------------  
-foobar      ClientAdmin        Client Administration                              System        Background   Activated          0                 1                                                  2009-09-04 18:31:20                                                      
-foobar      CommConfigMgr      Communications Configuration Manager               CommMgmt      Batch        Activated          0                 20            1                  1                 2009-09-04 18:37:49                                                      
-foobar      CommInboundMgr     Communications Inbound Manager                     CommMgmt      Batch        Activated          0                 20            1                  1                 2009-09-04 18:31:20                                                      
-foobar      CommOutboundMgr    Communications Outbound Manager                    CommMgmt      Batch        Activated          0                 50            1                  1                 2009-09-04 18:37:50                                                      
-foobar      CommSessionMgr     Communications Session Manager                     CommMgmt      Batch        Activated          0                 20            1                  1                 2009-09-04 18:31:20                                                      
-foobar      DbXtract           Database Extract                                   Remote        Batch        Activated          0                 50                                                 2009-09-04 18:31:20                                                      
-foobar      EAIObjMgr_ptb      EAI Object Manager (PTB)                           EAI           Interactive  Activated          0                 20            1                  1                 2009-09-04 18:31:20                                                      
-foobar      MailMgr            Email Manager                                      CommMgmt      Background   Activated          0                 20                                                 2009-09-04 18:31:20                                                      
-foobar      EIM                Enterprise Integration Mgr                         EAI           Batch        Activated          0                 5                                                  2009-09-04 18:31:20                                                      
-foobar      FSMSrvr            File System Manager                                System        Batch        Activated          0                 20            1                  1                 2009-09-04 18:31:20                                                      
-foobar      GenNewDb           Generate New Database                              Remote        Batch        Activated          0                 1                                                  2009-09-04 18:31:20                                                      
-foobar      GenTrig            Generate Triggers                                  Workflow      Batch        Activated          0                 1                                                  2009-09-04 18:31:20                                                      
-foobar      PageMgr            Page Manager                                       CommMgmt      Background   Activated          0                 20                                                 2009-09-04 18:31:20                                                      
-foobar      PDbXtract          Parallel Database Extract                          Remote        Batch        Running            4                 100           1                  10                2009-09-04 18:37:51                                                      
-foobar      ServerMgr          Server Manager                                     System        Interactive  Running            1                 20                                                 2009-09-04 18:31:20                                                      
-foobar      SRBroker           Server Request Broker                              System        Interactive  Running            20                100           1                  1                 2009-09-04 18:37:48                                                      
-foobar      SRProc             Server Request Processor                           System        Interactive  Running            2                 20            1                  1                 2009-09-04 18:37:45                                                      
-foobar      SynchMgr           Synchronization Manager                            Remote        Interactive  Activated          0                 100           2                  20                2009-09-14 17:25:14                                                      
-foobar      TxnMerge           Transaction Merger                                 Remote        Background   Running            1                 5                                                  2009-09-14 17:24:52                                                      
-foobar      TxnProc            Transaction Processor                              Remote        Background   Running            1                 1                                                  2009-09-04 18:31:20                                                      
-foobar      TxnRoute           Transaction Router                                 Remote        Background   Running            3                 5                                                  2009-09-14 17:24:22                                                      
-foobar      UpgKitBldr         Upgrade Kit Builder                                SiebAnywhere  Batch        Activated          0                 1             1                  1                 2009-09-04 18:37:53                                                      
-foobar      WorkActn           Workflow Action Agent                              Workflow      Background   Activated          0                 5                                                  2009-09-08 11:02:34                                                      
-foobar      WorkMon            Workflow Monitor Agent                             Workflow      Background   Running            1                 1                                                  2009-09-04 18:31:20                                                      
-foobar      WorkMonFOOBA       Workflow Monitor Agent FOOBAR                      Workflow      Background   Running            1                 1                                                  2009-09-04 18:31:20                                                      
-foobar      WorkMonBudget      Workflow Monitor Agent Budget                      Workflow      Background   Running            1                 1                                                  2009-09-08 10:58:41                                                      
-foobar      WorkMonMyEmail     Workflow Monitor Agent My Email                    Workflow      Background   Running            1                 1                                                  2009-09-04 18:31:20                                                      
-foobar      WorkMonPLUS        Workflow Monitor Agent PLUS                        Workflow      Background   Running            1                 1                                                  2009-09-04 18:31:20                                                      
-foobar      WfProcBatchMgr     Workflow Process Batch Manager                     Workflow      Batch        Activated          0                 20            1                  1                 2009-09-08 11:04:41                                                      
-foobar      WfProcMgr          Workflow Process Manager                           Workflow      Batch        Activated          0                 20            1                  1                 2009-09-08 11:03:23                                                      
-foobar      ePharmaObjMgr_ptb  ePharma Object Manager (PTB)                       LifeSciences  Interactive  Activated          0                 60            1                  2                 2009-09-17 17:17:25                                                      
-
-31 rows returned.
 
