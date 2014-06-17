e38fc142e38c57626a6e45ba012be51bf80b0872
alceu.freitas.jr@gmail.com <alceu.freitas.jr@gmail.com@f8da17be-bb99-043e-0d47-acc943b59c7d>
2014-06-17 02:40:20 +0000

    small fixes, specially to POD

--- Siebel-Srvrmgr/lib/Siebel/Srvrmgr/Daemon/Action.pm
+++ Siebel-Srvrmgr/lib/Siebel/Srvrmgr/Daemon/Action.pm
@@ -225,7 +225,7 @@ Alceu Rodrigues de Freitas Junior, E<lt>arfreitas@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 of Alceu Rodrigues de Freitas Junior, E<lt>arfreitas@cpan.org<E<gt>
+This software is copyright (c) 2012 of Alceu Rodrigues de Freitas Junior, E<lt>arfreitas@cpan.orgE<gt>
 
 This file is part of Siebel Monitoring Tools.
 
